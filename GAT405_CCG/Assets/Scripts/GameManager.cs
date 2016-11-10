6f27cc0636175fb8410cb606aee094e8d4be05e7
Josh Painter <j.painter@digipen.edu>
2016-11-10 11:56:35 -0800

    lol

---
+++ GAT405_CCG/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public const int MAX_CARDS = 20;
+    private Vector3 playerDeckLoc = new Vector3(15,-7,0);
+    private Vector3 enemyDeckLoc = new Vector3(15, 7, 0);
+    public GameObject card;
+
+    // Use this for initialization
+    void Start()
+    {
+        initializeDecks();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void initializeDecks()
+    {
+        // spawn player deck
+        for (int i = 0; i < MAX_CARDS; ++i)
+        {
+            GameObject spawnedCard = (GameObject)Instantiate(card, playerDeckLoc + new Vector3(i * 0.01f, 0, 0), transform.rotation);
+        }
+
+        // spawn opponent deck
+        for (int i = 0; i < MAX_CARDS; ++i)
+        {
+            GameObject spawnedCard = (GameObject)Instantiate(card, enemyDeckLoc + new Vector3(i * 0.01f, 0, 0), transform.rotation);
+        }
+    }
+}
